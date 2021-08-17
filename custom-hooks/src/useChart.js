b1fa62508559e9c6ea50b730dd4a08184859c6f7
Rasik Fareed <muhammed.rasik.795@gmail.com>
2021-08-17 13:51:38 +0530

    code clean up

--- custom-hooks/src/useChart.js
+++ custom-hooks/src/useChart.js
@@ -3,15 +3,6 @@ import Chart from "chart.js/auto";
 
 const useChart = (nodeRef, options) => {
   React.useEffect(() => {
-    // if (nodeRef && nodeRef.current) {
-    //   const chart = Chart.getChart(nodeRef.current.id);
-    //   if (chart) {
-    //     chart.destroy();
-    //   }
-    //   new Chart(nodeRef.current, options);
-    // } else {
-    //   new Chart(nodeRef.current, options);
-    // }
     const chart = Chart.getChart(nodeRef.current.id);
     if (chart) {
       chart.destroy();
